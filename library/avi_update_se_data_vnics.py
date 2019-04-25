4cba486160b4324ebefe01a522d78a128688b0e4
Chaitanya Deshpande <chaitanya.deshpande@avinetworks.com>
2019-04-25 22:12:47 +0530

    Added missing import

--- library/avi_update_se_data_vnics.py
+++ library/avi_update_se_data_vnics.py
@@ -101,7 +101,7 @@ from ansible.module_utils.basic import AnsibleModule
 try:
     from avi.sdk.avi_api import ApiSession, AviCredentials
     from avi.sdk.utils.ansible_utils import (
-        avi_common_argument_spec,
+        avi_ansible_api, avi_common_argument_spec,
         ansible_return)
     HAS_AVI = True
 except ImportError:
