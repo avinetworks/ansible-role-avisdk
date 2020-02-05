35aab6e7c80855a22b75606ffcbc965b7ea0abfe
karthikvas <karthik@avinetworks.com>
2020-02-05 03:49:39 +0000

    Assets updated for ansible sdk role

--- library/avi_gslb.py
+++ library/avi_gslb.py
@@ -248,7 +248,7 @@ EXAMPLES = """
         port: 283
         cluster_uuid: "cluster-d4ee5fcc-3e0a-4d4f-9ae6-4182bc605829"
 
-- name: Update Gslb site's configurations (Patch Delete Operation)
+- name: Delete Gslb site's den_vses configurations (Patch Delete(dns_vses) Operation)
   avi_gslb:
     avi_credentials:
       username: "{{ username }}"
@@ -263,6 +263,19 @@ EXAMPLES = """
     sites:
       - ip_addresses: "10.10.28.83"
       - ip_addresses: "10.10.28.86"
+
+- name: Delete Gslb complete site's configurations (Patch Delete(site) Operation)
+  avi_gslb:
+    avi_credentials: "{{ avi_credentials }}"
+    api_version: 18.2.8
+    avi_api_update_method: patch
+    avi_api_patch_op: delete
+    patch_level: '/site'
+    name: gslb.lab2.local
+    leader_cluster_uuid: "cluster-84aa795f-8f09-42bb-97a4-5103f4a53da9"
+    dns_configs:
+    sites:
+      - ip_addresses: 10.10.28.83
 """
 
 RETURN = '''
@@ -282,6 +295,82 @@ try:
 except ImportError:
     HAS_AVI = False
 
+def patch_add_gslb(module, gslb_obj):
+    sites = module.params['sites']
+    dns_configs = module.params.get("dns_configs", None)
+    if 'dns_configs' in gslb_obj:
+        gslb_obj['dns_configs'].extend(dns_configs)
+        gslb_obj['dns_configs'] = list({v['domain_name'] : v for v in
+                                        gslb_obj['dns_configs']}.values())
+    else:
+        gslb_obj['dns_configs'] = dns_configs
+    if sites:
+        for site in sites:
+            site_ips = site.get('ip_addresses', None)
+            if not site_ips:
+                return module.fail_json(msg=(
+                        "ip_addr of site %s in a configuration is mandatory. "
+                        "Please provide ip_addresses i.e. gslb site's ip." %
+                        module.params['name']))
+            current_gslb_sites = gslb_obj.get('sites', [])
+            for current_gslb_site in current_gslb_sites:
+                if current_gslb_site['name'] == site['name']:
+                    for key, val in site.items():
+                        if (key == 'dns_vses' and 'dns_vses' in
+                                current_gslb_site):
+                            current_gslb_site['dns_vses'].extend(val)
+                            current_gslb_site['dns_vses'] = list(
+                                {v['dns_vs_uuid']: v for v in
+                                 current_gslb_site['dns_vses']}.values())
+                        else:
+                            current_gslb_site[key] = val
+                    break
+            else:
+                gslb_obj['sites'].append(site)
+    return gslb_obj
+
+
+def patch_replace_gslb(module, gslb_obj):
+    sites = module.params['sites']
+    dns_configs = module.params.get("dns_configs", None)
+    if dns_configs:
+        gslb_obj['dns_configs'] = dns_configs
+    if sites:
+        for site in sites:
+            site_ips = site.get('ip_addresses', None)
+            if not site_ips:
+                return module.fail_json(msg=(
+                        "ip_addr of site %s in a configuration is mandatory. "
+                        "Please provide ip_addresses i.e. gslb site's ip." %
+                        module.params['name']))
+            current_gslb_sites = gslb_obj.get('sites', [])
+            for current_gslb_site in current_gslb_sites:
+                if current_gslb_site['name'] == site['name']:
+                    for key, val in site.items():
+                        current_gslb_site[key] = val
+    return gslb_obj
+
+
+def patch_delete_gslb(module, gslb_obj):
+    sites = module.params['sites']
+    gslb_obj['dns_configs'] = []
+    if sites:
+        for site in sites:
+            site_ips = site.get('ip_addresses', None)
+            if not site_ips:
+                return module.fail_json(msg=(
+                        "ip_addr of site %s in a configuration is mandatory. "
+                        "Please provide ip_addresses i.e. gslb site's ip." %
+                        module.params['name']))
+            current_gslb_sites = gslb_obj.get('sites', [])
+            for current_gslb_site in current_gslb_sites:
+                if site_ips == current_gslb_site['ip_addresses'][0]['addr']:
+                    if module.params['patch_level'] == '/site':
+                        gslb_obj['sites'].remove(current_gslb_site)
+                    else:
+                        current_gslb_site['dns_vses'] = []
+    return gslb_obj
+
 
 def main():
     argument_specs = dict(
@@ -290,6 +379,8 @@ def main():
         avi_api_update_method=dict(default='put',
                                    choices=['put', 'patch']),
         avi_api_patch_op=dict(choices=['add', 'replace', 'delete']),
+        patch_level=dict(type='str', default='/site/dns_vses',
+                         choices=['/site/dns_vses', '/site']),
         async_interval=dict(type='int',),
         clear_on_max_retries=dict(type='int',),
         client_ip_addr_group=dict(type='dict',),
@@ -323,65 +414,32 @@ def main():
         api_creds = AviCredentials()
         api_creds.update_from_ansible_module(module)
         api = ApiSession.get_session(
-            api_creds.controller, api_creds.username, password=api_creds.password,
-            timeout=api_creds.timeout, tenant=api_creds.tenant,
-            tenant_uuid=api_creds.tenant_uuid, token=api_creds.token,
-            port=api_creds.port)
+            api_creds.controller, api_creds.username,
+            password=api_creds.password, timeout=api_creds.timeout,
+            tenant=api_creds.tenant, tenant_uuid=api_creds.tenant_uuid,
+            token=api_creds.token, port=api_creds.port)
         # Get existing gslb objects
         rsp = api.get('gslb', api_version=api_creds.api_version)
         existing_gslb = rsp.json()
         gslb = existing_gslb['results']
-        sites = module.params['sites']
         for gslb_obj in gslb:
-            # Update/Delete domain names in dns_configs fields in gslb object.
-            if 'dns_configs' in module.params:
-                if gslb_obj['leader_cluster_uuid'] == module.params['leader_cluster_uuid']:
-                    if str(patch_op).lower() == 'delete':
-                        gslb_obj['dns_configs'] = []
-                    elif str(patch_op).lower() == 'add':
-                        if module.params['dns_configs'] not in gslb_obj['dns_configs']:
-                            gslb_obj['dns_configs'].extend(module.params['dns_configs'])
-                    else:
-                        gslb_obj['dns_configs'] = module.params['dns_configs']
-            # Update/Delete sites configuration
-            if sites:
-                for site_obj in gslb_obj['sites']:
-                    dns_vses = site_obj.get('dns_vses', [])
-                    for obj in sites:
-                        config_for = obj.get('ip_addresses', None)
-                        if not config_for:
-                            return module.fail_json(msg=(
-                                "ip_addr of site in a configuration is mandatory. "
-                                "Please provide ip_addresses i.e. gslb site's ip."))
-                        if config_for == site_obj['ip_addresses'][0]['addr']:
-                            if str(patch_op).lower() == 'delete':
-                                site_obj['dns_vses'] = []
-                            else:
-                                # Modify existing gslb sites object
-                                for key, val in obj.items():
-                                    if key == 'dns_vses' and str(patch_op).lower() == 'add':
-                                        found = False
-                                        # Check dns_vses field already exists on the controller
-                                        for v in dns_vses:
-                                            if val[0]['dns_vs_uuid'] != v['dns_vs_uuid']:
-                                                found = True
-                                                break
-                                        if not found:
-                                            dns_vses.extend(val)
-                                    else:
-                                        site_obj[key] = val
-                                if str(patch_op).lower() == 'add':
-                                    site_obj['dns_vses'] = dns_vses
-            uni_dns_configs = [dict(tupleized) for tupleized in set(tuple(item.items())
-                                                                    for item in gslb_obj['dns_configs'])]
-            gslb_obj['dns_configs'] = uni_dns_configs
+            if (gslb_obj['leader_cluster_uuid'] ==
+                    module.params['leader_cluster_uuid']):
+                if str(patch_op).lower() == 'add':
+                    patch_add_gslb(module, gslb_obj)
+                elif str(patch_op).lower() == 'replace':
+                    patch_replace_gslb(module, gslb_obj)
+                elif str(patch_op).lower() == 'delete':
+                    patch_delete_gslb(module, gslb_obj)
             module.params.update(gslb_obj)
-        module.params.update(
-            {
-                'avi_api_update_method': 'put',
-                'state': 'present'
-            }
-        )
+            module.params.pop("patch_level")
+            module.params.update(
+                {
+                    'avi_api_update_method': 'put',
+                    'state': 'present'
+                }
+            )
+
     return avi_ansible_api(module, 'gslb',
                            set([]))
 
