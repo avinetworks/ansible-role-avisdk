3e22a943e1d3a76f005001d61bf38fbd18f2fdbe
Shounak Joshi <shounak.joshi@avinetworks.com>
2020-08-19 20:44:41 -0700

    Assets updated for ansible sdk role

--- library/avi_serviceenginegroup.py
+++ library/avi_serviceenginegroup.py
@@ -1170,6 +1170,13 @@ options:
         description:
             - It is a reference to an object of type tenant.
         type: str
+    transient_shared_memory_max:
+        description:
+            - The threshold for the transient shared config memory in the se.
+            - Allowed values are 0-100.
+            - Field introduced in 18.2.10.
+            - Default value when not specified in API or module is interpreted by Avi Controller as 30.
+        type: int
     udf_log_throttle:
         description:
             - This setting limits the number of udf logs generated per second per core on this se.
@@ -1528,6 +1535,7 @@ def main():
         significant_log_throttle=dict(type='int',),
         ssl_preprocess_sni_hostname=dict(type='bool',),
         tenant_ref=dict(type='str',),
+        transient_shared_memory_max=dict(type='int',),
         udf_log_throttle=dict(type='int',),
         url=dict(type='str',),
         use_standard_alb=dict(type='bool',),
